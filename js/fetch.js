68d836cc81fd8651e5fa5b124dc2cfd3d631a060
jbjoybiswasjb <jbjoybiswasjb@gmail.com>
2024-04-02 20:13:05 +0600

    Show each AI data on UI.

--- js/fetch.js
+++ js/fetch.js
@@ -24,22 +24,22 @@ const showData = (apiData) => {
     apiDiv.innerHTML = `
     <div class="card bg-base-100 p-[1.5625em] border border-blackColor/10">
         <figure class="rounded-2xl">
-            <img src="images/chatgpt_assistant_1.png" alt="Chatgpt Assistant Image." title="Chat GPT Assistant Photo." />
+            <img src="${apiData?.image}" />
         </figure>
         <div class="card-body p-0">
             <h2 class="card-title work-sans-semi-bold mt-[1.5625em] mb-4 text-blackColor">Features</h2>
             <ol class="text-base text-textColor work-sans-regular list-decimal pl-[1.5625em]">
-                <li>Natural language processing</li>
-                <li>Contextual understanding</li>
-                <li>Text generation</li>
+                <li>${apiData?.features[0]}</li>
+                <li>${apiData?.features[1]}</li>
+                <li>${apiData?.features[2]}</li>
             </ol>
             <hr class="border border-blackColor/20 my-6">
             <div class="card-actions justify-between items-center">
                 <div>
-                    <h3 class="work-sans-semi-bold text-[1.5625em] text-blackColor mb-4">ChatGPT</h3>
+                    <h3 class="work-sans-semi-bold text-[1.5625em] text-blackColor mb-4">${apiData?.name}</h3>
                     <div class="text-base text-textColor work-sans-medium">
                         <span class="pr-[0.6875em]"><i class="far fa-calendar-alt"></i></span>
-                        <span>11/01/2022</span>
+                        <span>${apiData?.published_in}</span>
                     </div>
                 </div>
                 <button class="btn flex-nowrap h-[3.125em] w-[3.125em] text-arrowColor bg-arrowBg rounded-full">
