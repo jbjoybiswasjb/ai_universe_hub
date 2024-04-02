b9561f7027cf0c46c4c3c4bdbd899f55d95e3aec
jbjoybiswasjb <jbjoybiswasjb@gmail.com>
2024-04-02 18:18:14 +0600

    Load and get all api data from api.

---
+++ js/fetch.js
@@ -0,0 +1,8 @@
+// At first get data from api.
+const loadAiData = async() => {
+    const res = await fetch('https://openapi.programming-hero.com/api/ai/tools');
+    const data = await res.json();
+    const allApiData = data.data.tools;
+    
+    console.log(allApiData);
+};
